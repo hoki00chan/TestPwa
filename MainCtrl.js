463354c011d0a74ab8deaeba7bc4aad81c229665
hokig <hokig@HOKI>
2025-08-04 23:08:21 +0900

    [Modify]Login画面修正

--- MainCtrl.js
+++ MainCtrl.js
@@ -2,7 +2,7 @@ console.log("Main screen loaded");
 
 // PWA用：Service Worker登録（再度必要）
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("service-worker.js")
+  navigator.serviceWorker.register("service_worker.js")
     .then(() => console.log("Service Worker registered"))
     .catch(err => console.error("Service Worker error:", err));
 }
